2a0f3e0e7d16a490f1a843afbdf4370d628328ee
Voitenko Ilya <79865476+IlyaVoitenko@users.noreply.github.com>
2023-07-28 13:34:49 +0300

    added loading page

--- src/components/Home/Context/index.js
+++ src/components/Home/Context/index.js
@@ -3,7 +3,7 @@ import React from "react";
 const Context = () => {
   return (
     <div className="flex flex-col  items-center text-left mt-12 ">
-      <div className="w-3/6 mt-12">
+      <div className="w-3/5	 mt-12">
         <h1 className="text-2xl font-bold text-blue-500">Lorem ipsum</h1>
         <br></br>
         <span>
@@ -31,7 +31,7 @@ const Context = () => {
           takimata sanctus est Lorem ipsum dolor sit amet.
         </span>
       </div>
-      <div className="w-3/6  flex flex-col">
+      <div className="w-3/5	  flex flex-col">
         <h1 className="mt-5 text-xl  font-bold text-blue-500">
           Lorem ipsum dolor sit amet
         </h1>
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,10 +1,10 @@
 import React, { Suspense } from "react";
 import { Outlet } from "react-router-dom";
-
+import Loading from "../Loading";
 const Layout = () => {
   return (
     <>
-      <Suspense fallback={<div>Loading ...</div>}>
+      <Suspense fallback={<Loading />}>
         <Outlet />
       </Suspense>
     </>
