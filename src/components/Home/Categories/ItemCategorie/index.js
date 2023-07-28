36196476933df234137241337d0e45edcda0ad59
Voitenko Ilya <79865476+IlyaVoitenko@users.noreply.github.com>
2023-07-28 15:13:04 +0300

    changed style for button of categories

--- src/components/Home/Categories/ItemCategorie/index.js
+++ src/components/Home/Categories/ItemCategorie/index.js
@@ -6,8 +6,10 @@ const ItemCategorie = ({ item }) => {
       <img src={item.src} className="w-full" alt="" />
       <h3 className="mt-3 font-bold">{item.title}</h3>
       <span className="text-left mt-3">{item.text}</span>
-      <a href={item.link} className="text-left mt-3">
-        <button>info more</button>
+      <a href={item.link} className="text-left mt-3 ">
+        <button className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+          info more
+        </button>
       </a>
     </div>
   );
