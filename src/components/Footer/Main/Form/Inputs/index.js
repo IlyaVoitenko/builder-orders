1f3bf26931e96e6913a5ba9a49f38b16ceb13205
Voitenko Ilya <79865476+IlyaVoitenko@users.noreply.github.com>
2023-08-02 20:49:50 +0300

    fix bug with textarea

--- src/components/Footer/Main/Form/Inputs/index.js
+++ src/components/Footer/Main/Form/Inputs/index.js
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Inputs = () => {
+  const [surname, setSurname] = useState("");
+  const [email, setEmail] = useState("");
+  const [news, setNews] = useState("");
+
   return (
     <div>
       <div className=" flex flex-row justify-between mt-1">
         <input
+          onChange={({ target }) => setSurname(target.value)}
+          value={surname}
           placeholder="surname"
           className="pt-2 pb-2 pl-2 w-[17rem] mr-"
         ></input>
-        <input placeholder="email" className="pt-2 pb-2 pl-2 w-[17rem]"></input>
+        <input
+          onChange={({ target }) => setEmail(target.value)}
+          value={email}
+          placeholder="email"
+          className="pt-2 pb-2 pl-2 w-[17rem]"
+        ></input>
       </div>
-      <textarea rows="4" cols="74" className="pt-2 pb-2 pl-2 mt-5">
-        query
-      </textarea>
+      <textarea
+        rows="4"
+        placeholder="news"
+        cols="74"
+        value={news}
+        onChange={({ target }) => setNews(target.value)}
+        className="pt-2 pb-2 pl-2 mt-5"
+      ></textarea>
     </div>
   );
 };
