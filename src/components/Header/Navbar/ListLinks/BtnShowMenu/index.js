04d49da54907850b5e8759ab5e49ab8280a95014
Voitenko Ilya <79865476+IlyaVoitenko@users.noreply.github.com>
2023-08-06 18:56:57 +0300

    changed BtnShowMenu for modile

--- src/components/Header/Navbar/ListLinks/BtnShowMenu/index.js
+++ src/components/Header/Navbar/ListLinks/BtnShowMenu/index.js
@@ -9,25 +9,11 @@ const BtnShowMenu = () => {
       data-collapse-toggle="navbar-dropdown"
       type="button"
       onClick={() => dispatch(setIsShowMenu())}
-      className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white rounded-lg lg:hidden hover:bg-white focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-white dark:focus:ring-gray-600"
+      className="inline-flex items-center  w-10 h-10 justify-center  text-white rounded-lg lg:hidden hover:bg-white focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-white dark:focus:ring-gray-600"
       aria-controls="navbar-dropdown"
       aria-expanded="false"
     >
-      <svg
-        className="w-5 h-5"
-        aria-hidden="true"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 17 14"
-      >
-        <path
-          stroke="currentColor"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          d="M1 1h15M1 7h15M1 13h15"
-        />
-      </svg>
+      &#9776;
     </button>
   );
 };
