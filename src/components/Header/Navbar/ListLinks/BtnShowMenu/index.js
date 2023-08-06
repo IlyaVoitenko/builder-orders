a0dd101f32aaeae49af42516e6c18554a5105dcb
Voitenko Ilya <79865476+IlyaVoitenko@users.noreply.github.com>
2023-08-06 19:02:22 +0300

    try to fix show trigram in a btn show menu

--- src/components/Header/Navbar/ListLinks/BtnShowMenu/index.js
+++ src/components/Header/Navbar/ListLinks/BtnShowMenu/index.js
@@ -6,12 +6,9 @@ const BtnShowMenu = () => {
   const dispatch = useDispatch();
   return (
     <button
-      data-collapse-toggle="navbar-dropdown"
       type="button"
       onClick={() => dispatch(setIsShowMenu())}
-      className="inline-flex items-center  w-10 h-10 justify-center  text-white rounded-lg lg:hidden hover:bg-white focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-white dark:focus:ring-gray-600"
-      aria-controls="navbar-dropdown"
-      aria-expanded="false"
+      className="inline-flex items-center  w-10 h-10 justify-center  lg:hidden  "
     >
       &#9776;
     </button>
