c043373dccf647f9b39bea147dc5f82da1d9caf2
Voitenko Ilya <79865476+IlyaVoitenko@users.noreply.github.com>
2023-08-07 19:38:41 +0300

    added i18n to photo gallery page

--- src/components/PhotoGallery/Main/Title/index.js
+++ src/components/PhotoGallery/Main/Title/index.js
@@ -1,16 +1,14 @@
 import React from "react";
-
+import { useTranslation } from "react-i18next";
 const Title = () => {
+  const { t } = useTranslation();
   return (
-    <section className="w-[71%] ">
-      <h3 className="text-4xl  text-blue-500">Photo Gallery</h3>
+    <section className="w-[71%]  max-lg:w-[85%] max-lg:text-sm	">
+      <h3 className="text-4xl max-lg:text-2xl text-blue-500">
+        {t("photoGallery.title")}
+      </h3>
       <br></br>
-      <p>
-        Here we have collected some impressions for you. All photographs are
-        taken from objects we have recently worked on and are intended to give
-        you an idea of ​​the many possible uses for stucco, from classic to
-        contemporary.
-      </p>
+      <p>{t("photoGallery.context")}</p>
     </section>
   );
 };
