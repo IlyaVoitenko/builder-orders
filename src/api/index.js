fa73eaea6a5964100d3b1955bc7efae62458106c
Voitenko Illia <79865476+IlyaVoitenko@users.noreply.github.com>
2024-02-26 12:28:09 +0100

    updated packages and changed README file

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,11 @@
 import axios from "axios";
-const { REACT_APP_BASE_URL } = process.env;
+const { REACT_APP_BASE_URL_SERVER } = process.env;
+
 export const loadTranslation = async (language = "de") => {
   try {
-    const { data } = await axios.get(`${REACT_APP_BASE_URL}${language}.json`);
+    const { data } = await axios.get(
+      `${REACT_APP_BASE_URL_SERVER}${language}.json`
+    );
     return data;
   } catch (error) {
     throw Error(error);
